a56fa818b9552a68c4ba745aeb7223cacd8286ed
cscmx <sainz.carolina@gmail.com>
2026-06-09 13:55:27 +0200

    Refactor: ejercicio2_S105 creado

---
+++ ejercicio2_S105.php
@@ -0,0 +1,19 @@
+<?php
+/*Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea de la figura.
+*mateix exercici que a POO1, però aquí necessitem que ho resolguis aplicant algun dels conceptes del tema POO2.
+*/
+
+require_once 'class_ejercicio2_S105/classShape.php';
+require_once 'class_ejercicio2_S105/classTriangle.php';
+require_once 'class_ejercicio2_S105/classRectangle.php';
+
+
+ $myTriangle = new Triangle(10,5);
+ $myRectangle = new Rectangle (5, 8);
+
+ echo "Area triangulo: ".$myTriangle->calcularArea().PHP_EOL;
+ echo "Area rectángulo: ".$myRectangle->calcularArea().PHP_EOL;
+
+
+ 
+ ?>
